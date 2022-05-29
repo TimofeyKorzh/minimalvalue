3f97c09c91405c64517139f70b8510070da70817
MyLittleCoin <mylittlecoin1@gmail.com>
2022-05-30 00:14:34 +0300

    cleared code, text updates

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import { TextField } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
-//import Button from './components/Button';
-import Button from '@material-ui/core/Button'
+
+
 import {MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './styles.scss';
-import { postGenerateTextEndpoint } from './utils';
-//import {Helmet} from "react-helmet";
 
-import CircularProgress from '@material-ui/core/CircularProgress'
-import { withStyles } from '@material-ui/core/styles'
 import { Container } from '@material-ui/core';
-import { Box } from '@material-ui/core';
+
 const TITLE = 'Нищета';
 function App() {
-  const [toggle, setToggle] = useState(false);
-  const [text, setText] = useState("");
-  //const [model, setModel] = useState('gpt2');
-  const model = "345M";
-  const [temperature, setTemperature] = useState(1);
-  const [lenght, setLenght] = useState(20);
-  const [generatedText, postGenerateText] = postGenerateTextEndpoint();
 
   const [RUB, setRub] = useState(109);
+  const inflation_rate = 1.28;
 
-  const handleChange = (event) => {
-    setText(event.target.value);
-  };
 
 
   useEffect(() => {
@@ -42,63 +28,23 @@ function App() {
     },
  });
 
- const styles = {
-  root: {
-    marginLeft: 5
-  }
-}
-const SpinnerAdornment = withStyles(styles)(props => (
-  <CircularProgress
-    className={props.classes.spinner}
-    size={10}
-    style={{marginLeft: "0.5em"}}
-  />
-))
-const AdornedButton = (props) => {
-  const {
-    children,
-    loading,
-    ...rest
-  } = props
-  return (
-    <Button size="large" style={{ marginTop: '1em', marginBottom: '1em', width: 'fit-content', paddingBottom: '2em', backgroundColor: 'transparent'}}
 
-    color="primary"
-    {...rest}>
-       
-      {children}
-      {loading && <SpinnerAdornment  {...rest} />}
-      
-    </Button>
-  )
-}
- 
-  const generateText = () => {
-    generatedText.complete = false;
-    postGenerateText({ text, temperature, lenght });
-    setToggle(false);
-  }
-  if (generatedText.complete && !generatedText.error && !toggle){
-    setText(text+generatedText.data.result);
-    setToggle(true);
-  }
-  //var RUB = 109;
-
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
 
 
   useEffect(() => {
-
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+  
     fetch("https://api.currencyapi.com/v3/latest?apikey=DLiY9BRrZHRKE51b0KvuYwImfC7XmSpN9K7XkrSB", requestOptions)
     .then(function(response){
       return response.json();
     })
     .then(function(result){
-      console.log(result);
-      setRub(result.data.RUB.value)
+      console.log(result.data.RUB.value);
+
+      setRub(result.data.RUB.value*inflation_rate)
 
     })
   
@@ -111,18 +57,19 @@ const AdornedButton = (props) => {
     
     <MuiThemeProvider theme ={THEME}>
     <div className='app-container'>
-      
+    <Container maxWidth="sm">
     <div className='centering'> 
-        <p>x {" < "} {Math.round(2.3*RUB)}k  лютейшая нищета на грани выживания</p>
+        <p>x {" < "} {Math.round(2.3*RUB)}k - лютейшая нищета на грани выживания</p>
         <p>{Math.round(2.3*RUB)}k {" < "} x {" < "} {Math.round(6.2*RUB)}k - обычная нищета, ипотека на 20 лет, кино и рестораны раз в неделю, еда из ашана</p>
-        <p>{Math.round(6.2*RUB)}k {" < "} x {" < "} {Math.round(10.8*RUB)}k - нормальная жизнь, ипотека на 10 лет, машина среднего класса (B, C)</p>
-        <p>{Math.round(10.8*RUB)}k {" < "} x {" < "} {Math.round(15.6*RUB)}k - хорошая жизнь, ипотека на 5 лет, путешествия</p>
-        <p>x {" > "} {Math.round(15.6*RUB)}k - достаточно хорошая жизнь, ипотека на 2 года, машина бизнес класса, девушка</p>
+        <p>{Math.round(6.2*RUB)}k {" < "} x {" < "} {Math.round(10.8*RUB)}k - нормальная жизнь, счет в армянском или грузинском банке, внж, ипотека на 10 лет, машина среднего класса (B, C)</p>
+        <p>{Math.round(10.8*RUB)}k {" < "} x {" < "} {Math.round(15.6*RUB)}k - хорошая жизнь,счет в европейском или американском банке, европейский паспорт, ипотека на 5 лет, путешествия</p>
+        <p>x {" > "} {Math.round(15.6*RUB)}k - достаточно хорошая жизнь, офшоры на Кипре, американский паспорт, ипотека на 2 года, машина бизнес класса, девушка</p>
         <p></p>
         <p><i>Сделано <a href="https://monetka.name">Монетой</a>, для канала <a href="https://t.me/lovedeathtransformers">Alex Wortega</a></i></p>
         
         
     </div>
+    </Container>
     </div>
     </MuiThemeProvider>
   );
